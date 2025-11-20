2b70af5e06e5f7edc9a4a71a108ef9159a915326
Joel Cohen <joelbcohen@gmail.com>
2025-11-20 08:03:40 -0500

    Initial Commit

---
+++ Cloud Crypto Watch App/Cloud_CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cloud_CryptoApp.swift
+//  Cloud Crypto Watch App
+//
+//  Created by Joel Cohen on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Cloud_Crypto_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
